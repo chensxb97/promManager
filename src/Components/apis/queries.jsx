423e1b1277172c90b94f8289ef6c1875578c231e
Benedict <chensxb97@gmail.com>
2024-08-25 13:49:31 +0800

    init config for ui

--- src/Components/apis/queries.jsx
+++ src/Components/apis/queries.jsx
@@ -1,17 +1,10 @@
-// import PocketBase from 'pocketbase';
 async function getQueries() {
-    // const db = new PocketBase('http://127.0.0.1:8090');
-    // const result = await db.records.getList('promQueries');
     const res = await fetch('http://127.0.0.1:8090/api/collections/promQueries/records?page=1&perPage=30', { cache: 'no-store' });
     const data = await res.json();
     return data?.items
 }
 
 async function getQuery(queryId) {
-    // const db = new PocketBase('http://127.0.0.1:8090');
-    // await db.records.getOne('promQueries', {
-    //   id: queryId
-    // });
     const res = await fetch(
         `http://127.0.0.1:8090/api/collections/promQueries/records/${queryId}`, { cache: 'no-store' }
     );
@@ -20,11 +13,6 @@ async function getQuery(queryId) {
 }
 
 async function createQuery(payload) {
-    // const db = new PocketBase('http://127.0.0.1:8090');
-    // await db.records.create('notes', {
-    // query,
-    // description
-    // });
     const { query, description } = payload
     await fetch('http://127.0.0.1:8090/api/collections/promQueries/records', {
         method: 'POST',
@@ -39,12 +27,6 @@ async function createQuery(payload) {
 }
 
 async function editQuery(payload) {
-    // const db = new PocketBase('http://127.0.0.1:8090');
-    // await db.records.update('promQueries', {
-    // id: queryId,
-    // query,
-    // description
-    // });
     const { queryId, query, description } = payload
     await fetch(`http://127.0.0.1:8090/api/collections/promQueries/records/${queryId}`, {
         method: 'PATCH',
@@ -59,10 +41,6 @@ async function editQuery(payload) {
 }
 
 async function deleteQuery(queryId) {
-    // const db = new PocketBase('http://127.0.0.1:8090');
-    // await db.records.delete('promQueries', {
-    //   id: queryId
-    // });
     await fetch(`http://127.0.0.1:8090/api/collections/promQueries/records/${queryId}`, {
         method: 'DELETE',
         headers: {
